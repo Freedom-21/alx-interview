67c854a40456f989ae8cd339ec29fa1969fb1362
Freedom-21 <yenesewfreedom8@gmail.com>
2024-10-26 00:30:35 +0200

    final

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -5,7 +5,16 @@ import sys
 
 if __name__ == '__main__':
     total_file_size = 0
-    status_code_counts = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
+    status_code_counts = {
+        "200": 0,
+        "301": 0,
+        "400": 0,
+        "401": 0,
+        "403": 0,
+        "404": 0,
+        "405": 0,
+        "500": 0
+    }
     valid_status_codes = set(status_code_counts.keys())
     line_count = 0
 
