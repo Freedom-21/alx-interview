f157a9d5eb753c92d53b4d6d2c5a09841952dd73
Freedom-21 <yenesewfreedom8@gmail.com>
2024-12-09 19:01:23 +0100

    first ver

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Module for making change
+"""
+
+def makeChange(coins, total):
+    """
+    Determines the fewest number of coins needed to meet a given total.
+
+    Args:
+        coins (list): List of integers representing coin denominations.
+        total (int): The total amount for which change is to be made.
+
+    Returns:
+        int: Fewest number of coins needed to meet the total.
+             Returns -1 if the total cannot be met by any combination of the coins.
+    """
+    if total <= 0:
+        return 0
+
+    if not coins:
+        return -1
+
+    dp = [total + 1] * (total + 1)
+    dp[0] = 0
+
+    for coin in coins:
+        if coin <= 0:
+            continue
+        for i in range(coin, total + 1):
+            if dp[i - coin] + 1 < dp[i]:
+                dp[i] = dp[i - coin] + 1
+
+    return dp[total] if dp[total] <= total else -1
